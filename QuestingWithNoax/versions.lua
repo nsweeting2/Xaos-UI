76a8f804051cc43af6e83097b40051fd71d54aa9
nsweeting2 <55107062+nsweeting2@users.noreply.github.com>
2022-02-10 13:48:03 -0500

    cleaning

--- QuestingWithNoax/versions.lua
+++ QuestingWithNoax/versions.lua
@@ -1,4 +1,4 @@
 -- ensure latest version listed here is the same one as in the script
 
-return {{[1] = [[2.0]],[2] = [[2.1]],},
+return {{[1] = [[1.0]],[2] = [[1.1]],[3] = [[2.0]],[4] = [[2.1]],},
 }
